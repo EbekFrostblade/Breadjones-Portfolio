a968d39b7282db86846faa633ffb9dbb83debfd6
Brad Jones <bradster.jones@gmail.com>
2018-11-11 15:28:39 -0600

    Eject build scripts, add SASS preprocessing
    As I learned I could do from this awesome source:
    https://medium.com/@oreofeolurin/configuring-scss-with-react-create-react-app-1f563f862724

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import './third-party/fontawesome/css/all.css';
 
 import BottomNav from './bottom-nav/bottom-nav.js';
